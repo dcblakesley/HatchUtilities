c3e40658ce1a676fd963374b50c0ca54f5e7821e
David Blakesley <dblakesley@webstaurantstore.com>
2023-12-05 16:06:14 -0700

    Add project files.

---
+++ HatchUtilities/CategoryHierarchyMigrationHelper.cs
@@ -0,0 +1,122 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Hatch.Core.Features.CategoryHierarchy.Models;
+using Hatch.Core.Features.Projects.RequestAndResponse;
+
+namespace HatchUtilities;
+
+public class CategoryHierarchyMigrationHelper
+{
+
+    /// <summary> Generate a spreadsheet showing the differences between the IDS CH and the existing Hatch CH </summary>
+    public async Task CompareHatchVsIdsCh(string bearerToken)
+    {
+        var client = new HttpClient();
+        var serializerOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+        var columnsMajorCategories = new List<List<string>>();
+        var columnsIdsCategories = new List<List<string>>();
+
+        // Get a list of all Projects from Hatch
+        var getProjectsResponse = await client.GetFromJsonAsync<GetProjectsResponse>("https://hatch-api.clarkinc.biz/api/Projects/GetProjectsForProjectList", serializerOptions);
+
+        // Get a list of all MajorCategoryIds from the projects
+        var majorCategoryIdsBeingUsedByHatch = getProjectsResponse!.Projects.Select(x => x.MajorCategoryId).Distinct().OrderBy(x => x).ToList();
+
+
+        var idsChDetailsResponse = await client.GetFromJsonAsync<IdsGetCategoryDetailsResponse>("https://idspurchasingapi.dev.clarkinc.biz/categoryhierarchy/get-category-details", serializerOptions);
+        var kk = idsChDetailsResponse!.Result;
+
+        // Get Hatch Category Hierarchy
+        var hatchCategoryHierarchy = await client.GetFromJsonAsync<NormalizedCategoryHierarchy>("https://hatch-api.clarkinc.biz/api/CategoryHierarchy/GetNormalizedCategoryHierarchy?useClauthEmployeeId=true", serializerOptions);
+
+        var allHatchMajorCategoryNames = hatchCategoryHierarchy!.MajorCategories.Select(x => x.Name.Trim()).OrderBy(x => x).ToList();
+        allHatchMajorCategoryNames.Insert(0, $"All Hatch Major Categories - {allHatchMajorCategoryNames.Count}");
+        columnsMajorCategories.Add(allHatchMajorCategoryNames);
+
+        // Get all the Major Category names from Hatch that are being used by Projects
+        var hatchMajorCategoryNames = hatchCategoryHierarchy!.MajorCategories.Where(x=> majorCategoryIdsBeingUsedByHatch.Contains(x.Id)).Select(x => x.Name.Trim()).OrderBy(x => x).ToList();
+        hatchMajorCategoryNames.Insert(0, $"Utilized Hatch Major Categories - {hatchMajorCategoryNames.Count}");
+        columnsMajorCategories.Add(hatchMajorCategoryNames);
+
+        // Get all the Major Category names from IDS
+        var idsMajorCategoryNames = idsChDetailsResponse.Result!.MajorCategories!.Select(x => x.CategoryName.Trim()).OrderBy(x => x).ToList();
+        idsMajorCategoryNames.Insert(0, $"All IDS Major Categories - {idsMajorCategoryNames.Count}");
+        columnsMajorCategories.Add(idsMajorCategoryNames);
+
+        // Check to see which Major Categories are missing from IDS
+        var hatchMajorCategoriesThatDontExistInIds = hatchMajorCategoryNames.Except(idsMajorCategoryNames, StringComparer.OrdinalIgnoreCase).ToList();
+        hatchMajorCategoriesThatDontExistInIds.Insert(0, $"Majors that don't exist in IDS - {hatchMajorCategoriesThatDontExistInIds.Count}");
+        columnsMajorCategories.Add(hatchMajorCategoriesThatDontExistInIds);
+
+
+        // IDS Categories
+
+        // Get a list of all IdsCategoryIds from the projects
+        var hatchUtilizedIdsCategoryIds = new List<int>();
+        foreach (var project in getProjectsResponse.Projects)
+        {
+            if (project.IdsCategoryIds == null || !project.IdsCategoryIds.Any())
+                continue;
+
+            hatchUtilizedIdsCategoryIds.AddRange(project.IdsCategoryIds);
+        }
+        hatchUtilizedIdsCategoryIds = hatchUtilizedIdsCategoryIds.Distinct().OrderBy(x => x).ToList();
+        
+        // All Hatch IdsCategoryCodes
+        var allHatchIdsCategoryCodes = hatchCategoryHierarchy.IdsCategories.Select(x => x.Code.Trim()).OrderBy(x => x).ToList();
+        allHatchIdsCategoryCodes.Insert(0, $"All Hatch IDS Category Codes - {allHatchIdsCategoryCodes.Count}");
+        columnsIdsCategories.Add(allHatchIdsCategoryCodes);
+
+        // Utilized Hatch IdsCategoryCodes
+        var hatchUtilizedIdsCategoryCodes = hatchCategoryHierarchy.IdsCategories.Where(x => hatchUtilizedIdsCategoryIds.Contains(x.Id)).Select(x => x.Code.Trim()).OrderBy(x => x).ToList();
+        hatchUtilizedIdsCategoryCodes.Insert(0, $"Utilized Hatch IDS Category Codes - {hatchUtilizedIdsCategoryCodes.Count}");
+        columnsIdsCategories.Add(hatchUtilizedIdsCategoryCodes);
+
+        // All IDS IdsCategoryCodes
+        var getIdsCategoriesResponse = await client.GetFromJsonAsync<IdsGetCategoryHierarchyResponse>("https://idspurchasingapi.dev.clarkinc.biz/categoryhierarchy/get-category-hierarchy", serializerOptions);
+        var idsCategoriesResults = getIdsCategoriesResponse!.Result!.CategoryHierarchies;
+        
+        // From IDS
+        var allIdsCategoryCodesFromIds = idsCategoriesResults!.Select(x => x.ItemCategoryCode.Trim()).OrderBy(x => x).ToList();
+        allIdsCategoryCodesFromIds.Insert(0, $"All CategoryCodes in IDS - {allIdsCategoryCodesFromIds.Count}");
+        columnsIdsCategories.Add(allIdsCategoryCodesFromIds);
+
+        // Utilized in Hatch, but don't exist in IDS
+        var idsCategoryCodesThatDontExistInHatch = hatchUtilizedIdsCategoryCodes.Except(allIdsCategoryCodesFromIds, StringComparer.OrdinalIgnoreCase).ToList();
+        idsCategoryCodesThatDontExistInHatch.Insert(0, $"IDS Categories utilized by Hatch, but don't exist in IDS - {idsCategoryCodesThatDontExistInHatch.Count}");
+        columnsIdsCategories.Add(idsCategoryCodesThatDontExistInHatch);
+
+        // Export the results to Excel
+        var builder = new WorkbookBuilder();
+        builder.AddSheet("Major Categories", columnsMajorCategories, true, true);
+        builder.AddSheet("IDS Categories", columnsIdsCategories, true, true);
+        builder.Save("CH Comparison.xlsx");
+    }
+
+    public async Task CreateHatchChFromIdsCh(string bearerToken)
+    {
+        var client = new HttpClient();
+        var serializerOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+
+        var idsGetCategoryDetailsResponse = (await client.GetFromJsonAsync<IdsGetCategoryDetailsResponse>("https://idspurchasingapi.dev.clarkinc.biz/categoryhierarchy/get-category-details", serializerOptions))?.Result;
+        var idsGetCategoriesResponse = (await client.GetFromJsonAsync<IdsGetCategoryHierarchyResponse>("https://idspurchasingapi.dev.clarkinc.biz/categoryhierarchy/get-category-hierarchy", serializerOptions))?.Result;
+
+
+
+        //var nch = new NormalizedCategoryHierarchy();
+
+        // Get Hatch Category Hierarchy
+        //var hatchCategoryHierarchy = await client.GetFromJsonAsync<NormalizedCategoryHierarchy>("https://hatch-api.clarkinc.biz/api/CategoryHierarchy/GetNormalizedCategoryHierarchy?useClauthEmployeeId=true", serializerOptions);
+
+        //var ch = new NormalizedCategoryHierarchy();
+
+
+    }
+}
+
+// 
+// public async Task UpdateCategoryHierarchyAsync(CancellationToken cancellationToken)
+// => await CategoryHierarchyService.UpdateCategoryHierarchyAsync(cancellationToken);
---
+++ HatchUtilities/WorkbookBuilder.cs
@@ -0,0 +1,133 @@
+﻿using System.Text.Json;
+using IronXL;
+
+namespace HatchUtilities;
+
+public class WorkbookBuilder
+{
+    WorkBook? _workBook;
+    private static List<string>? _columnLetters;
+
+    // Public Methods
+    public void CreateWorkbook() => _workBook = new WorkBook(ExcelFileFormat.XLSX);
+    public MemoryStream ToStream() => _workBook.ToStream();
+    public void Save(string path) => _workBook.SaveAs(path);
+
+    public void AddSheet(string name, List<List<object>> rowsOrColumns, bool autosize = false, bool transpose = false)
+    {
+        if (_workBook == null)
+        {
+            CreateWorkbook();
+        }
+
+        var workSheet = transpose
+            ? AddSheetByColumns(name, rowsOrColumns)
+            : AddSheetByRows(name, rowsOrColumns);
+
+        if (autosize)
+        {
+            AutoSizeAllColumns(workSheet);
+        }
+    }
+
+    public void AddSheet(string name, List<List<string>> rowsOrColumns, bool autosize = false, bool transpose = false)
+        => AddSheet(name, StringsToObjects(rowsOrColumns), autosize, transpose);
+
+    // Private Methods
+    private static void AutoSizeAllColumns(WorkSheet workSheet)
+    {
+        foreach (var column in workSheet.Columns)
+        {
+            column.AutoSizeColumn();
+        }
+    }
+    private static List<List<object>> StringsToObjects(List<List<string>> stringLists)
+    {
+        var objectLists = new List<List<object>>();
+        foreach (var column in stringLists)
+        {
+            objectLists.Add(column.Select(x => (object)x).ToList());
+        }
+
+        return objectLists;
+    }
+    private static List<string> GetColumnLetters()
+    {
+        if (_columnLetters != null)
+        {
+            return _columnLetters;
+        }
+
+        var lettersString = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+        var letters = lettersString.Split(",");
+        var l2 = JsonSerializer.Deserialize<List<string>>(JsonSerializer.Serialize(letters));
+
+        var columnLetters = new List<string>();
+
+        // A - Z, 26
+        var singles = lettersString.Split(",");
+
+        // AA - ZZ, 702
+        var doubles = new List<string>();
+        foreach (var letter in letters)
+        {
+            doubles.AddRange(l2!.Select(l => letter + l));
+        }
+
+        // AAA - ZZZ, 18278
+        var triples = new List<string>();
+        foreach (var two in doubles)
+        {
+            triples.AddRange(letters.Select(l => two + l));
+        }
+
+        columnLetters.AddRange(singles);
+        columnLetters.AddRange(doubles);
+        columnLetters.AddRange(triples);
+
+        _columnLetters = columnLetters;
+        return columnLetters;
+    }
+
+    /// <summary> Send in a list of Rows </summary>
+    private WorkSheet AddSheetByRows(string name, List<List<object>> rows)
+    {
+        var columnLetters = GetColumnLetters();
+        var workSheet = _workBook.CreateWorkSheet(name);
+
+        var rowCounter = 0;
+
+        foreach (var row in rows)
+        {
+            var columnCounter = 0;
+            rowCounter++;
+            foreach (var o in row)
+            {
+                workSheet[$"{columnLetters[columnCounter++]}{rowCounter}"].Value = o;
+            }
+        }
+
+        return workSheet;
+    }
+
+    /// <summary> Send in a list of Columns </summary>
+    private WorkSheet AddSheetByColumns(string name, List<List<object>> columns)
+    {
+        var columnLetters = GetColumnLetters();
+        var workSheet = _workBook.CreateWorkSheet(name);
+
+        var columnCounter = 0;
+
+        foreach (var column in columns)
+        {
+            var rowCounter = 1;
+            foreach (var o in column)
+            {
+                workSheet[$"{columnLetters[columnCounter]}{rowCounter++}"].Value = o;
+            }
+            columnCounter++;
+        }
+
+        return workSheet;
+    }
+}
